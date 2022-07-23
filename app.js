164f27d24e7910ed256b1b67e849f7372c79a45c
YOOYONGHYUN <yyh901@naver.com>
2022-07-23 23:00:56 +0900

    js활용하여 css 동적추가

--- app.js
+++ app.js
@@ -1,28 +1,10 @@
 import StarRating from "./star-rating/index.js";
-
-document
-  .querySelector("head")
-  .insertAdjacentHTML(
-    "beforeEnd",
-    `<link href="star-rating/theme.css" rel="stylesheet" />`
-  );
-
-let children = document.querySelector("head").children;
-for (let i = 0; i < children.length; i++) {
-  if (children[i].localName == "link")
-    console.log(`${i}번째 바로 다음에 링크를 만들어야 하는데 어케 만들지`);
-}
-
-console.log(document.querySelector("head").querySelectorAll("link")[1]);
-console.log(HTMLCollection.arguments);
-console.log(document.querySelector("head"));
-console.log(document.querySelector("head").children);
-
-console.log(document.querySelector("head").children.length);
+import addLink from "./star-rating/link.js";
 
 const $containers = [...document.querySelectorAll(".star-rating")];
 const $currentRatings = document.querySelectorAll(".current-rating > span");
 
+addLink($containers);
 $containers.forEach(($container, i) => {
   // star-rating 컨테이너 요소의 참조를 StarRating 함수에 전달해 star 요소들로 구성된 star-rating 요소를 동적 생성한다.
   StarRating($container);
