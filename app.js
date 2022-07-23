1f4b31c8e113edc3747acf53eabc20c33b06b618
YOOYONGHYUN <yyh901@naver.com>
2022-07-23 22:15:32 +0900

    별 평점 매기기

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
-import StarRating from './star-rating/index.js';
+import StarRating from "./star-rating/index.js";
 
-const $containers = [...document.querySelectorAll('.star-rating')];
-const $currentRatings = document.querySelectorAll('.current-rating > span');
+document
+  .querySelector("head")
+  .insertAdjacentHTML(
+    "beforeEnd",
+    `<link href="star-rating/theme.css" rel="stylesheet" />`
+  );
+
+let children = document.querySelector("head").children;
+for (let i = 0; i < children.length; i++) {
+  if (children[i].localName == "link")
+    console.log(`${i}번째 바로 다음에 링크를 만들어야 하는데 어케 만들지`);
+}
+
+console.log(document.querySelector("head").querySelectorAll("link")[1]);
+console.log(HTMLCollection.arguments);
+console.log(document.querySelector("head"));
+console.log(document.querySelector("head").children);
+
+console.log(document.querySelector("head").children.length);
+
+const $containers = [...document.querySelectorAll(".star-rating")];
+const $currentRatings = document.querySelectorAll(".current-rating > span");
 
 $containers.forEach(($container, i) => {
   // star-rating 컨테이너 요소의 참조를 StarRating 함수에 전달해 star 요소들로 구성된 star-rating 요소를 동적 생성한다.
   StarRating($container);
-
+  console.log(i);
   // 이벤트 'rating-change'를 캐치해 화면에 표시한다.
-  $container.addEventListener('rating-change', e => {
+  $container.addEventListener("rating-change", (e) => {
     const rating = e.detail;
     $currentRatings[i].textContent = rating;
   });
--- star-rating/index.js
+++ star-rating/index.js
@@ -1,1 +1,72 @@
 // do something!
+function StarRating($container) {
+  //   let link = document.createElement("link");
+  //   link.href = "star-rating/theme.css";
+  //   link.rel = "stylesheet";
+  //   document.querySelector("head").append(link);
+  //   document
+  //     .querySelector("head")
+  //     .insertAdjacentHTML(
+  //       "beforeEnd",
+  //       `<link href="star-rating/theme.css" rel="stylesheet" />`
+  //     );
+
+  let div = document.createElement("div");
+  div.classList.add("star-rating-container");
+  $container.appendChild(div);
+
+  console.log($container.dataset.maxRating);
+  let starNum = $container.dataset.maxRating;
+
+  let count = 0;
+  for (let i = 0; i < starNum; i++) {
+    let icon = document.createElement("i");
+    //클래스 한번에 주는 방법 찾아보기.
+    icon.classList.add("bx");
+    icon.classList.add("bxs-star");
+    $container.querySelector(".star-rating-container").appendChild(icon);
+
+    $container
+      .querySelectorAll(".bx")
+      [i].addEventListener("mouseover", function () {
+        //5번째에 마우스를 올리면 5번째까지 다 색칠되어야 한다... 흐음....
+        console.log(i);
+        for (let j = 0; j < i + 1; j++) {
+          $container.querySelectorAll(".bx")[j].classList.add("hovered");
+        }
+      });
+
+    $container
+      .querySelectorAll(".bx")
+      [i].addEventListener("mouseout", function () {
+        for (let j = 0; j < i + 1; j++) {
+          $container.querySelectorAll(".bx")[j].classList.remove("hovered");
+        }
+      });
+
+    $container
+      .querySelectorAll(".bx")
+      [i].addEventListener("click", function () {
+        //4번째걸 클릭헸다가 2번째걸 클릭하면 2번째까지만 색칠되어야.,,, 이건 또 어찌할꼬.. 흠...
+        for (let j = 0; j <= i; j++) {
+          while (count > i) {
+            $container
+              .querySelectorAll(".bx")
+              [count].classList.remove("selected");
+            count--;
+          }
+
+          $container.querySelectorAll(".bx")[j].classList.add("selected");
+          count = j;
+        }
+
+        let currentRating = String(i + 1);
+        let change = new CustomEvent("rating-change", {
+          detail: currentRating,
+        });
+        $container.dispatchEvent(change);
+      });
+  }
+}
+
+export default StarRating;
